5a2992e7c1dc78768d7b95219d2db0ceee1ed68b
droksty <droksty@gmail.com>
2023-06-10 09:17:50 +0300

    import bootstrap and gen welcomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './welcome/welcome.component';
 
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  // { path: 'insert-form', component: InsertFormComponent },
+  // { path: 'res-form', component: ResFormComponent },
+  // { path: 'res-form/:reference', component: ResFormComponent },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
