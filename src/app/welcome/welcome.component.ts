3ac978957078fe45c99d10a99ba3c769b743f309
droksty <droksty@gmail.com>
2023-06-10 10:12:03 +0300

    move searchByReference input to welcomeComponent

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+  
+  searchForm: FormGroup;
+  
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      reference: ['', Validators.required]
+    })
+  }
 
+  findOneByReference() {
+    if (this.searchForm.valid) {
+      // console.log(this.searchForm.value.reference)
+      this.router.navigate(['/res-form/' + this.searchForm.value.reference])
+    } else {
+      console.log("not valid")
+    }
+    
+    // this.searchForm.reset();
+  }
 }
