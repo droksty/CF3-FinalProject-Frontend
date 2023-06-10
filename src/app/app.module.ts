d5a5ee402a5fa9dd51638a3529c7630583a7601d
droksty <droksty@gmail.com>
2023-06-10 09:24:21 +0300

    gen page-not-found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,21 @@ import { RouterModule, Routes} from '@angular/router'
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   // { path: 'insert-form', component: InsertFormComponent },
   // { path: 'res-form', component: ResFormComponent },
   // { path: 'res-form/:reference', component: ResFormComponent },
-  // { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
